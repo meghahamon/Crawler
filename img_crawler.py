53a0b801b8946bd3b8383751bde7281a50756040
Megha K Sabu <megha@hamon.in>
2022-03-18 16:05:55 +0530

    download the image

--- img_crawler.py
+++ img_crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests as rq
+import wget
 header = {
     'User-Agent': 'Mozilla/5.0 (MacintoshIntel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
@@ -17,12 +18,20 @@ def get_image(site_url):
     return ret
 
 
+def image_download(img_url):
+    r = rq.get(url=img_url, headers=header)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    image_div = soup.find("img", {"class": "AssetCard-module__image___dams4"})
+    x = image_div['src']
+    print(x, "\n")
+    return x
+
+
 def main():
     img_link = get_image("https://www.gettyimages.in/photos/aamir-khan-actor")
-    head = "https://www.gettyimages.in"
-    for i in img_link:
-        print(head+i,
-              "\n")
+    base_link = "https://www.gettyimages.in"
+    for link in img_link:
+        wget.download(image_download(base_link+link))
 
 
 if __name__ == "__main__":
