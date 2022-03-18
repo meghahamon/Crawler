98e02d3d759b1c8e097043c6c9716ff96cd06f72
Megha K Sabu <megha@hamon.in>
2022-03-18 14:52:44 +0530

    function for getting an image from a url

---
+++ img_crawler.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests as rq
+header = {
+    'User-Agent': 'Mozilla/5.0 (MacintoshIntel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
+
+def get_image(site_url):
+    ret = []
+    r = rq.get(url=site_url, headers=header)
+    soup = BeautifulSoup(r.content, features="html.parser")
+    gallery_img = soup.find(
+        "div", {"class": "GalleryItems-module__searchContent___DbMmK"})
+    link = gallery_img.find_all("a")
+    for i in link:
+        ret.append((i['href']))
+    return ret
+
+
+def main():
+    amir = get_image("https://www.gettyimages.in/photos/aamir-khan-actor")
+
+
+if __name__ == "__main__":
+    main()
